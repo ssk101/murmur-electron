d06e31a67adbfc02c4237e357caf4656cb302587
Stian Skjondal <ssk@scoutgg.com>
2019-12-02 20:19:37 +0100

    hid args

--- luxafor.py
+++ luxafor.py
@@ -7,15 +7,30 @@ print(platform.system())
 vendor = 0x04d8
 product = 0xf372
 
+mac = dict({
+  "red": [0, 1, 0xFF, 255, 0, 0],
+  "green": [0, 1, 0xFF, 0, 255, 0],
+  "blue": [0, 1, 0xFF, 0, 0, 255],
+  "yellow": [0, 1, 0xFF, 255, 255, 0],
+})
+linux = dict({
+  "red": 82,
+  "green": 71,
+  "blue": 66,
+  "yellow": 89
+})
+
 if platform.system() == "Darwin":
   import hid
   dev = hid.device(vendor, product)
   dev.open(vendor, product)
   dev.send_feature_report([0x02, 0x10, 0x00,0x00,0x00,0x00,0x00,0x00])
+  colors = mac
 elif platform.system() == "Linux":
   import usb.core
   import usb.util
   dev = usb.core.find(idVendor = vendor, idProduct = product)
+  colors = linux
   if dev is None:
     raise ValueError('Device not found')
   try:
@@ -25,21 +40,10 @@ elif platform.system() == "Linux":
   dev.set_configuration()
 
 
-colors = dict({
-  "red": 82,
-  "green": 71,
-  "blue": 66,
-  "yellow": 89
-})
-
 def set_color(color):
-  dev.write(1, [0, 0])
-  dev.write(1, [0, colors[color]])
-  print(colors[color])
-  # "red" == 82
-  # "green" == 71
-  # "blue" == 66
-  # "yellow" == 89
+  if(platform.system() == 'Linux'):
+    dev.write(1, [0, 0])
+  dev.write(colors[color])
 
 if __name__ == "__main__":
   print(sys.argv)
